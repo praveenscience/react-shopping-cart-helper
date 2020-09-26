83f05c6bb5dd73bd610f2f774a2070f02bfccf8d
Praveen Kumar <praveenscience@gmail.com>
2020-09-26 13:42:01 +0100

    Displaying the list of items as actionable elements.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Header from "./Header";
+import Items from "../constants/Items";
+import ItemsList from "./ItemsList";
 
 const App = () => {
   return (
@@ -7,6 +9,13 @@ const App = () => {
       <Header className="Header d-block text-center mb-4" dark={true}>
         React Shopping Helper
       </Header>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12">
+            <ItemsList Items={Items} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
